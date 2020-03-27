2cfdce1f5eb8c27d8495040614befcb936170152
shengkai <Administrator@DESKTOP-539KRNO>
2020-03-27 14:08:20 +0800

    main

---
+++ abstract/main.cpp
@@ -0,0 +1,28 @@
+#include<opencv2\opencv.hpp>
+#include<iostream>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+	VideoCapture cap;
+	cap.open(0);
+	while (1) {
+		cv::Mat src,dst;
+		bool rSucess = cap.read(src);
+		if (rSucess) {
+			
+			medianBlur(src, dst, 3);
+			imshow("src", src);
+			imshow("dst", dst);
+			waitKey(0);
+		}
+			
+		
+	}
+
+
+
+
+
+}
